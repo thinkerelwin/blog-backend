d8ba677575ebebc1d12a43bc0f48697b01ee6e90
Elwin <thinkerelwin@gmail.com>
2021-03-08 22:38:13 +0800

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4c8287ab75f388b35cc2b711d22d96cb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7e0c5bea-dcb8-4003-af30-b3650137c774'
+};
